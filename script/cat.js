d96e8ac179bc76951606112d4ff123e11d434a7c
myithelper <714525329@qq.com>
2019-06-17 18:16:58 +0900

    2

---
+++ script/cat.js
@@ -0,0 +1,72 @@
+
+
+function includeCatagoryJp(){
+var catagory =
+'<h6>車用</h6>'
++'<ul>'
++  '<li><a href="detail/bingxiang.html">車載 冷凍庫</a></li>'
++  '<li><a href="detail/fengshan.html">扇風機 手持ち</a></li>'
++  '<li><a href="detail/kongqijinghuaji.html">空気清浄機</a></li>'
++'</ul>'
+
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
+
+
+function includeCatagoryKo(){
+var catagory =
+
+
+'<h6>차량용</h6>'
++  '<ul>'
+  +  '<li><a href="detail/bingxiang.html">차량용 냉동고</a></li>'
+  +  '<li><a href="detail/fengshan.html">선풍기 손잡이</a></li>'
+  +  '<li><a href="detail/kongqijinghuaji.html">공기 세척기</a></li>'
+  +'</ul>'
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
+
+
+
+function includeCatagoryDetailJp(){
+var catagory =
+'<h6>車用</h6>'
++'<ul>'
++  '<li><a href="../detail/bingxiang.html">車載 冷凍庫</a></li>'
++  '<li><a href="../detail/fengshan.html">扇風機 手持ち</a></li>'
++  '<li><a href="../detail/kongqijinghuaji.html">空気清浄機</a></li>'
++'</ul>'
+
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
+
+
+
+function includeCatagoryDetailKo(){
+var catagory =
+
+
+'<h6>차량용</h6>'
++  '<ul>'
+  +  '<li><a href="../detail/bingxiang.html">차량용 냉동고</a></li>'
+  +  '<li><a href="../detail/fengshan.html">선풍기 손잡이</a></li>'
+  +  '<li><a href="../detail/kongqijinghuaji.html">공기 세척기</a></li>'
+  +'</ul>'
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
