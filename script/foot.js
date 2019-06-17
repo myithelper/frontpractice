c366ad2ddc32384741cb5286e315d79dc2952b3d
myithelper <714525329@qq.com>
2019-06-17 19:00:09 +0900

    2

---
+++ script/foot.js
@@ -0,0 +1,8 @@
+function includeFoot(){
+var foot = '<div class="footer-bottom text-center"><div class="container"><div class="row"><div class="col-md-12 col-sm-12 col-xs-12"><div class="copyright"><p>Copyright &copy; 2017.Company name All rights reserved.</p></div></div></div></div></div>'
+
+
+
+var div = document.getElementById("foot");
+div.innerHTML = foot
+}
