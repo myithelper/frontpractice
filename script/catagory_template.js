f814c7f60d870e72337376d8de69fc09bf7a321f
myithelper <714525329@qq.com>
2019-07-08 17:58:13 +0900

    1

---
+++ script/catagory_template.js
@@ -0,0 +1,65 @@
+
+
+function includeCatagoryJp(){
+var catagory =
+'<h6>車用</h6>'
++'<ul>'
++'cat_jp'
++'</ul>'
+
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
+
+
+function includeCatagoryKo(){
+var catagory =
+
+
+'<h6>차량용</h6>'
++  '<ul>'
++'cat_ko'
+  +'</ul>'
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
+
+
+
+function includeCatagoryDetailJp(){
+var catagory =
+'<h6>車用</h6>'
++'<ul>'
++'cat_detail_jp'
++'</ul>'
+
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
+
+
+
+function includeCatagoryDetailKo(){
+var catagory =
+
+
+'<h6>차량용</h6>'
++  '<ul>'
++'cat_detail_ko'
+
+  +'</ul>'
+
+
+
+var div = document.getElementById("catagory");
+div.innerHTML = catagory
+}
