e8b7febed2ea32232df7809b74c3f604f7431ccc
myithelper <714525329@qq.com>
2019-07-08 19:11:12 +0900

    1

--- script/catagory.js
+++ script/catagory.js
@@ -1,8 +1,6 @@
-
-
 function includeCatagoryJp(){
 var catagory =
-'<h6>車用</h6>'
+'<h6>新規</h6>'
 +'<ul>'
 +'<li><a href="detail/bingxiang.html">車載 冷凍庫</a></li><li><a href="detail/fengshan.html">扇風機 手持ち</a></li><li><a href="detail/kongqijinghuaji.html">空気清浄機</a></li>'
 +'</ul>'
@@ -19,7 +17,7 @@ function includeCatagoryKo(){
 var catagory =
 
 
-'<h6>차량용</h6>'
+'<h6>새로운</h6>'
 +  '<ul>'
 +'<li><a href="detail/bingxiang.html">차량용 냉동고</a></li><li><a href="detail/fengshan.html">선풍기 손잡이</a></li><li><a href="detail/kongqijinghuaji.html">공기 세척기</a></li>'
   +'</ul>'
@@ -34,7 +32,7 @@ div.innerHTML = catagory
 
 function includeCatagoryDetailJp(){
 var catagory =
-'<h6>車用</h6>'
+'<h6>新規</h6>'
 +'<ul>'
 +'<li><a href="../detail/bingxiang.html">車載 冷凍庫</a></li><li><a href="../detail/fengshan.html">扇風機 手持ち</a></li><li><a href="../detail/kongqijinghuaji.html">空気清浄機</a></li>'
 +'</ul>'
@@ -52,7 +50,7 @@ function includeCatagoryDetailKo(){
 var catagory =
 
 
-'<h6>차량용</h6>'
+'<h6>새로운</h6>'
 +  '<ul>'
 +'<li><a href="../detail/bingxiang.html">차량용 냉동고</a></li><li><a href="../detail/fengshan.html">선풍기 손잡이</a></li><li><a href="../detail/kongqijinghuaji.html">공기 세척기</a></li>'
 
