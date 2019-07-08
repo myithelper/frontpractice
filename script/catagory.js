0579169320a29e93fc09cbf472cd7d334453bfe7
myithelper <714525329@qq.com>
2019-07-08 20:02:46 +0900

    1

--- script/catagory.js
+++ script/catagory.js
@@ -1,8 +1,10 @@
+
+
 function includeCatagoryJp(){
 var catagory =
 '<h6>新規</h6>'
 +'<ul>'
-+'<li><a href="detail/bingxiang.html">車載 冷凍庫</a></li><li><a href="detail/fengshan.html">扇風機 手持ち</a></li><li><a href="detail/kongqijinghuaji.html">空気清浄機</a></li>'
++'<li><a href="detail/bingxiang.html">車載 冷凍庫</a></li><li><a href="detail/fengshan.html">扇風機 手持ち</a></li><li><a href="detail/kongqijinghuaji.html">空気清浄機</a></li><li><a href="detail/google3d.html">google 3d メガネ</a></li>'
 +'</ul>'
 
 
@@ -19,7 +21,7 @@ var catagory =
 
 '<h6>새로운</h6>'
 +  '<ul>'
-+'<li><a href="detail/bingxiang.html">차량용 냉동고</a></li><li><a href="detail/fengshan.html">선풍기 손잡이</a></li><li><a href="detail/kongqijinghuaji.html">공기 세척기</a></li>'
++'<li><a href="detail/bingxiang.html">차량용 냉동고</a></li><li><a href="detail/fengshan.html">선풍기 손잡이</a></li><li><a href="detail/kongqijinghuaji.html">공기 세척기</a></li><li><a href="detail/google3d.html">google 3d 안경</a></li>'
   +'</ul>'
 
 
@@ -34,7 +36,7 @@ function includeCatagoryDetailJp(){
 var catagory =
 '<h6>新規</h6>'
 +'<ul>'
-+'<li><a href="../detail/bingxiang.html">車載 冷凍庫</a></li><li><a href="../detail/fengshan.html">扇風機 手持ち</a></li><li><a href="../detail/kongqijinghuaji.html">空気清浄機</a></li>'
++'<li><a href="../detail/bingxiang.html">車載 冷凍庫</a></li><li><a href="../detail/fengshan.html">扇風機 手持ち</a></li><li><a href="../detail/kongqijinghuaji.html">空気清浄機</a></li><li><a href="../detail/google3d.html">google 3d メガネ</a></li>'
 +'</ul>'
 
 
@@ -52,7 +54,7 @@ var catagory =
 
 '<h6>새로운</h6>'
 +  '<ul>'
-+'<li><a href="../detail/bingxiang.html">차량용 냉동고</a></li><li><a href="../detail/fengshan.html">선풍기 손잡이</a></li><li><a href="../detail/kongqijinghuaji.html">공기 세척기</a></li>'
++'<li><a href="../detail/bingxiang.html">차량용 냉동고</a></li><li><a href="../detail/fengshan.html">선풍기 손잡이</a></li><li><a href="../detail/kongqijinghuaji.html">공기 세척기</a></li><li><a href="../detail/google3d.html">google 3d 안경</a></li>'
 
   +'</ul>'
 
