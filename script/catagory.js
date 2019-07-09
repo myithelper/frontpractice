40ea72087264bf06249ef61605d7be94e9139b24
myithelper <714525329@qq.com>
2019-07-09 13:52:57 +0900

    1

--- script/catagory.js
+++ script/catagory.js
@@ -19,7 +19,7 @@ function includeCatagoryKo(){
 var catagory =
 
 
-'<h6>새로운</h6>'
+'<h6>새로운상품</h6>'
 +  '<ul>'
 +'<li><a href="detail/google3d.html">google 3d 안경</a></li><li><a href="detail/kongqijinghuaji.html">공기 세척기</a></li><li><a href="detail/fengshan.html">선풍기 손잡이</a></li><li><a href="detail/bingxiang.html">차량용 냉동고</a></li>'
   +'</ul>'
@@ -52,11 +52,10 @@ function includeCatagoryDetailKo(){
 var catagory =
 
 
-'<h6>새로운</h6>'
+'<h6>새로운상품</h6>'
 +  '<ul>'
 +'<li><a href="../detail/google3d.html">google 3d 안경</a></li><li><a href="../detail/kongqijinghuaji.html">공기 세척기</a></li><li><a href="../detail/fengshan.html">선풍기 손잡이</a></li><li><a href="../detail/bingxiang.html">차량용 냉동고</a></li>'
-
-  +'</ul>'
++'</ul>'
 
 
 
